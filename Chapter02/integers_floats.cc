c2e6d18e1da1f14ede482928e0cf6e4db24c33e0
Ericesposito <eespo93@optonline.net>
2024-04-10 21:37:28 -0400

    playing around with data types in C++

---
+++ Chapter02/integers_floats.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdint>
+
+int main()
+{
+
+    std::int8_t i1 = 0; // Signed 8bit
+    std::uint8_t i2 = 0U; // Unsigned 8bit
+
+    std::int16_t i3 = 0; // Signed 16bit
+    std::uint16_t i4 = 0U; // Unsigned 16bit
+
+    std::int32_t i5 = 0; // Signed 32bit
+    std::uint32_t i6 = 0U; // Unsigned 32bit
+
+    std::int64_t i7 = 0; // Signed 64bit
+    std::uint64_t i8 = 0U; // Unsigned 64bit
+
+    float f1 = 12.0F; // 32bit
+    double d1 = 12.0; // 64bit
+
+    return 0;
+}
